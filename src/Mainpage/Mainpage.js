92c8a19c945570899c40bcefd74802210d30ecc2
hyeswe10 <jmoon981107@gmail.com>
2026-03-26 14:05:40 +0900

    메뉴바 이동

--- src/Mainpage/Mainpage.js
+++ src/Mainpage/Mainpage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import TopMenu from '../components/TopMenu';
 import AboutMe from '../components/AboutMe';
 import MainProjects from '../components/MainProjects';
@@ -6,13 +6,25 @@ import CloneCoding from '../components/CloneCoding';
 import PracticalPublishing from '../components/PracticalPublishing';
 
 const Mainpage = () => {
+    const aboutRef = useRef(null);
+    const mainRef = useRef(null);
+    const cloneRef = useRef(null);
+    const practicalRef = useRef(null);
+    const scrollTo = (ref)=>{
+        ref.current?.scrollIntoView({behavior: 'smooth'})
+    }
     return (
         <div id='main-page'>
-            <TopMenu/>
-            <AboutMe/>
-            <MainProjects/>
-            <CloneCoding/>
-            <PracticalPublishing/>
+            <TopMenu
+                onAbout={()=>{scrollTo(aboutRef)}}
+                onMain={()=>{scrollTo(mainRef)}}
+                onClone={()=>{scrollTo(cloneRef)}}
+                onPractical={()=>{scrollTo(practicalRef)}}
+            />
+            <div ref={aboutRef}><AboutMe/></div>
+            <div ref={mainRef}><MainProjects/></div>
+            <div ref={cloneRef}><CloneCoding/></div>
+            <div ref={practicalRef}><PracticalPublishing/></div>
         </div>
     );
 };
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { TiHomeOutline } from "react-icons/ti";
 
-const TopMenu = () => {
+const TopMenu = ({ onAbout, onMain, onClone, onPractical }) => {
     return (
         <div id="top-menu">
             <div className="home-btn"><TiHomeOutline /></div>
             <div className="menu-list">
-                <a data-text="ABOUT ME">ABOUT ME</a>
-                <a data-text="MAIN PROJECTS">MAIN PROJECTS</a>
-                <a data-text="CLONE CODING">CLONE CODING</a>
-                <a data-text="PRACTICAL">PRACTICAL</a>
+                <a onClick={onAbout} data-text="ABOUT ME">ABOUT ME</a>
+                <a onClick={onMain} data-text="MAIN PROJECTS">MAIN PROJECTS</a>
+                <a onClick={onClone} data-text="CLONE CODING">CLONE CODING</a>
+                <a onClick={onPractical} data-text="PRACTICAL">PRACTICAL</a>
             </div>
         </div>
     );
